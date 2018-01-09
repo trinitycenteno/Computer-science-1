3cbfe42cb299ef8ebd2896f422ba4f085ef37bed
trinitycenteno <34751532+trinitycenteno@users.noreply.github.com>
2018-01-09 14:40:36 -0500

    Add files via upload

---
+++ Welcome to my gmail.py
@@ -0,0 +1,12 @@
+print("welcome to gmail")
+myUsername =input("Enter your username, then press Return: ")
+
+if myUsername=="johndoe":
+    print("Username successful")
+    myPassword==input("enter your password, then press Return: ")
+    if myPassword=="sof123@":
+        print("success")
+    else:
+        print("unable to log in. Incorrect password")
+else:
+    print("Incorrect. Try again.")
