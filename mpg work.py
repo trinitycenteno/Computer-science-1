106147e060e35a5b8defe70d98a125474107c206
trinitycenteno <34751532+trinitycenteno@users.noreply.github.com>
2018-01-30 13:33:59 -0500

    Add files via upload

---
+++ mpg work.py
@@ -0,0 +1,13 @@
+'''
+build a calculator that provides the amount of miles per gallon
+
+miles per gallon = miles driven/gallons used
+
+'''
+
+print("this program calculates mpg.")
+miles_driven = float(input("please enter the amount of miles driven:"))
+gallons_used = float(input("enter gallons used:"))
+mpg= miles_driven / gallons_used
+
+print("your miles per gallon is", mpg)
