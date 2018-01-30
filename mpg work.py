8cf510eca9f0f61e97ce37e63621f7c9046b3fde
trinitycenteno <34751532+trinitycenteno@users.noreply.github.com>
2018-01-30 13:42:16 -0500

    Add files via upload

--- mpg work.py
+++ mpg work.py
@@ -5,9 +5,15 @@ miles per gallon = miles driven/gallons used
 
 '''
 
-print("this program calculates mpg.")
-miles_driven = float(input("please enter the amount of miles driven:"))
-gallons_used = float(input("enter gallons used:"))
-mpg= miles_driven / gallons_used
-
-print("your miles per gallon is", mpg)
+while True: 
+    print("this program calculates mpg.")
+    miles_driven = float(input("please enter the amount of miles driven:"))
+    gallons_used = float(input("enter gallons used:"))
+    mpg= miles_driven / gallons_used
+    print("your miles per gallon is", mpg)
+    user_input= input("would you like to perform a new calculation? Y/N")
+    if user_input== "Y" or user_input=="y":
+        continue
+    else:
+        break
+    
